e83d291349c7f687ccbca4ff309cc4f5ee52406d
Juliany Fernandes <jufernanders@gmail.com>
2024-08-28 03:03:39 -0300

    Verifies user request processing

--- src/controller/createUser.go
+++ src/controller/createUser.go
@@ -1,12 +1,24 @@
 package controller
 
 import (
+	"fmt"
+
 	"github.com/gin-gonic/gin"
 	"github.com/julianyf/Desafio_BrandMonitor/src/configuration/rest_err"
+	"github.com/julianyf/Desafio_BrandMonitor/src/controller/model/request"
 )
 
 func CreateUser(c *gin.Context) {
 
-	err := rest_err.NewBadRequestError("Voce chamou a rota de forma errada")
-	c.JSON(err.Code, err)
+	var UserRequest request.UserRequest
+
+	if err := c.ShouldBindJSON(&UserRequest); err != nil {
+		restErr := rest_err.NewBadRequestError(
+			fmt.Sprintf("There are some incorrect filds, error=%s", err.Error()))
+
+		c.JSON(restErr.Code, restErr)
+		return
+	}
+
+	fmt.Println(UserRequest)
 }
