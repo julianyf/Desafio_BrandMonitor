7e9664954a5b8e786dfe715c09a7049bafc9dad6
Juliany Fernandes <jufernanders@gmail.com>
2024-08-29 12:07:20 -0300

    Adds info and error logger

--- src/controller/createUser.go
+++ src/controller/createUser.go
@@ -1,25 +1,42 @@
 package controller
 
 import (
-	"fmt"
-	"log"
+	"net/http"
 
 	"github.com/gin-gonic/gin"
+	"github.com/julianyf/Desafio_BrandMonitor/src/configuration/logger"
 	"github.com/julianyf/Desafio_BrandMonitor/src/configuration/validation"
 	"github.com/julianyf/Desafio_BrandMonitor/src/controller/model/request"
+	"github.com/julianyf/Desafio_BrandMonitor/src/controller/model/response"
+	"go.uber.org/zap"
 )
 
 func CreateUser(c *gin.Context) {
-	log.Println("Init CreateUser controller")
+	logger.Info("Init CreateUser controller",
+		zap.String("journey", "createUser"),
+	)
 	var UserRequest request.UserRequest
 
 	if err := c.ShouldBindJSON(&UserRequest); err != nil {
-		log.Printf("Error trying to marshal object, error=%s\n", err.Error())
+		logger.Error("Error trying to validate user info", err,
+			zap.String("journey", "createUser"),
+		)
 		errRest := validation.ValidateUserError(err)
 
 		c.JSON(errRest.Code, errRest)
 		return
 	}
 
-	fmt.Println(UserRequest)
+	response := response.UserResponse{
+		ID:    "test",
+		Email: UserRequest.Email,
+		Name:  UserRequest.Name,
+		Age:   UserRequest.Age,
+	}
+
+	logger.Info("User created sucessfully",
+		zap.String("journey", "createUser"),
+	)
+
+	c.JSON(http.StatusOK, response)
 }
